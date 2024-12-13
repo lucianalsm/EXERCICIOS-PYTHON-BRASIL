7bea8cd4e2c02579aa4ad31a0c16cb421b5b77b7
Luciana Miguel <luciana_miguel2004@hotmail.com>
2024-12-13 19:23:30 -0300

    exercicios em python

--- exercicio1.py
+++ exercicio1.py
@@ -6,3 +6,4 @@ tamanho_string= len(texto)
 
 while i<tamanho_string:
     print(texto[i])
+    i+=1
--- exercicio3.py
+++ exercicio3.py
@@ -1,3 +1,12 @@
 #faça um programa que peça 2 números e imprima a soma
 
 numero_1= int(input("Digite um número:"))
+      
+numero_2= int(input('Digite outro número:')) 
+
+soma_nr= numero_1 + numero_2
+
+print(f'A soma dos números é = {soma_nr}')
+
+
+
